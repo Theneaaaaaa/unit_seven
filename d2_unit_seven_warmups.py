eeead21d1d2264d56e21db10a8792205a04da211
Thenea <thenea@iMac4.local>
2018-10-29 14:35:07 -0400

    basic string methods

---
+++ d2_unit_seven_warmups.py
@@ -0,0 +1,16 @@
+name = "Guido van Rossum"
+
+lowercase_name = name.lower()
+
+print(lowercase_name)
+
+index = name.index("v")
+
+print(index)
+
+print(name[10])
+
+split_name = lowercase_name.split()
+
+print(split_name)
+
